a204ca8dc2335d9cada717092ed7dd771d5c7385
samueldahl <samjoedahl@gmail.clom>
2017-12-09 15:36:40 -0700

    Got some help from my friend JD, I still don't fully understand what is going on.

--- script.js
+++ script.js
@@ -1,9 +1,70 @@
+var data = {latitude: 40.296898, longitude:-111.694647, radius:50};
 
-var data = {ContentType: application/json, latitude: 40.296898, Longitude:-111.694647, radius:50};
-var experiment;
+function loadCourses() {
+    // create instance of xhttp
+    var xhttp = new XMLHttpRequest();
+    // set xhttp callback for whenever the state of the connection has moved on
+    xhttp.onreadystatechange = function() {
+        // checks if state is finished
+        if (this.readyState === 4 && this.status === 200) {
+            // parses response and reads from JSON
+            var courses = JSON.parse(this.responseText).courses;
+            // logs the data from the response that you needed
+            console.log(courses);
+        }
+    };
+    // set connection type and location
+    xhttp.open('POST', 'http://golf-courses-api.herokuapp.com/courses', true);
+    // adds headers that the server will read
+    xhttp.setRequestHeader('Content-Type', 'application/json');
+    // send headers and 'data' as body to the server
+    xhttp.send(JSON.stringify(data));
+}
 
-$.post('http://golf-courses-api.herokuapp.com/courses/search',  function (data, status){
-    experiment = JSON.parse(data);
-    console.log(experiment);
-});
+loadCourses();
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// $.post('http://golf-courses-api.herokuapp.com/courses', data,  function (data, status){
+//     experiment = JSON.parse(data);
+//     console.log(experiment);
+// });
